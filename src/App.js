23b94d316b54745a33f98574a392ba4efed97ed4
Sergey Antipov <antip99999@mail.ru>
2022-09-08 13:10:53 +0300

    Finish landing(add drawer) and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,73 @@
 function App() {
 	return (
 		<div className='wrapper clear'>
+			<div className='overlay'>
+				<div className='drawer'>
+					<div className='d-flex justify-between'>
+						<h2 className='mb-30'>Корзина</h2>
+						<img
+							className='cu-p'
+							src='/img/close.svg'
+							alt='close'
+							width={32}
+							height={32}
+						/>
+					</div>
+
+					<div className='items'>
+						<div className='cartItem d-flex align-center mb-20'>
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+								className='cartItemImg'
+							></div>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<button className='button removeBtn'>
+								<img src='/img/close.svg' alt='close' width={32} height={32} />
+							</button>
+						</div>
+						<div className='cartItem d-flex align-center mb-20'>
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+								className='cartItemImg'
+							></div>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<button className='button removeBtn'>
+								<img src='/img/close.svg' alt='close' width={32} height={32} />
+							</button>
+						</div>
+					</div>
+					<div className='cartTotalBlock'>
+						<ul>
+							<li>
+								<span>Итого:</span>
+								<div></div>
+								<b>21 498 руб. </b>
+							</li>
+							<li>
+								<span>Налог 5%: </span>
+								<div></div>
+								<b>1074 руб. </b>
+							</li>
+						</ul>
+						<button className='greenButton button'>
+							Оформить заказ{' '}
+							<img
+								src='/img/arrow.svg'
+								alt='arrow'
+								width={14}
+								height={12}
+							></img>
+						</button>
+					</div>
+				</div>
+			</div>
+
 			<header className='d-flex justify-between align-center p-40'>
 				<div className='d-flex align-center'>
 					<img
@@ -64,7 +131,7 @@ function App() {
 								<span>Цена:</span>
 								<b>12 999 руб.</b>
 							</div>
-							<button>
+							<button className='button'>
 								<img
 									src='img/not-added-item.svg'
 									alt='plus'
@@ -95,7 +162,7 @@ function App() {
 								<span>Цена:</span>
 								<b>12 999 руб.</b>
 							</div>
-							<button>
+							<button className='button'>
 								<img
 									src='img/not-added-item.svg'
 									alt='plus'
@@ -126,7 +193,7 @@ function App() {
 								<span>Цена:</span>
 								<b>8 499 руб.</b>
 							</div>
-							<button>
+							<button className='button'>
 								<img
 									src='img/not-added-item.svg'
 									alt='plus'
@@ -157,7 +224,7 @@ function App() {
 								<span>Цена:</span>
 								<b>8 999 руб.</b>
 							</div>
-							<button>
+							<button className='button'>
 								<img
 									src='img/not-added-item.svg'
 									alt='plus'
