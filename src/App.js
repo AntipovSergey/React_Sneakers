6a3aee1b3c8e34fa8d2dcad948fcc71485a1269c
Sergey Antipov <antip99999@mail.ru>
2022-09-08 14:55:01 +0300

    implement rendering cards from array of objects with using props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import Drawer from './components/Drawer';
 import Header from './components/Header';
 
 function App() {
+	const arr = [
+		{
+			title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+			price: 12999,
+			imageUrl: '/img/sneakers/1.jpg',
+		},
+		{
+			title: 'Мужские Кроссовки Nike Air Max 270',
+			price: 12999,
+			imageUrl: '/img/sneakers/2.jpg',
+		},
+		{
+			title: 'Мужские Кроссовки Nike Blazer Mid',
+			price: 8490,
+			imageUrl: '/img/sneakers/3.jpg',
+		},
+		{
+			title: 'Кроссовки Puma X Aka Boku Future Rider',
+			price: 8999,
+			imageUrl: '/img/sneakers/4.jpg',
+		},
+	];
 	return (
 		<div className='wrapper clear'>
 			<Drawer />
@@ -16,11 +38,14 @@ function App() {
 					</div>
 				</div>
 				<div className='d-flex flex-wrap'>
-					<Card
-						title={'Мужские Кроссовки Nike Blazer Mid Suede'}
-						price={12999}
-						imageUrl={'/img/sneakers/1.jpg'}
-					/>
+					{arr.map((obj, index) => (
+						<Card
+							key={index}
+							title={obj.title}
+							price={obj.price}
+							imageUrl={obj.imageUrl}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
